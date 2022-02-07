36036a7d0d91c6617bbd510bc41a1073d6867781
Ryan Wolf <ryantwolf1@gmail.com>
2022-02-06 18:57:31 -0600

    Make masks from pngs

--- synthetic/mask_maker.py
+++ synthetic/mask_maker.py
@@ -5,12 +5,13 @@ import os
 def make_masks(image_dir, mask_dir):
     # Go through each image in image_dir
     for image_name in os.listdir(image_dir):
-        # Read in the image
-        image = cv2.imread(os.path.join(image_dir, image_name))
-        # Create a mask, where any pixel in image that is not black is 255 in mask
-        mask = np.where(image != [0, 0, 0], 255, 0)
-        # Save the mask
-        cv2.imwrite(os.path.join(mask_dir, image_name), mask)
+        if image_name.endswith(".png"):
+            # Read in the image
+            image = cv2.imread(os.path.join(image_dir, image_name))
+            # Create a mask, where any pixel in image that is not black is 255 in mask
+            mask = np.where(image != [0, 0, 0], 255, 0)
+            # Save the mask
+            cv2.imwrite(os.path.join(mask_dir, image_name), mask)
 
 if __name__ == '__main__':
     image_dir = r'/media/ryan/DATA/HDR_Surface_Reconstruction/my_data/hotdog_tonemapped_0/image'
